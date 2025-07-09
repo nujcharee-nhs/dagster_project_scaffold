72900cdfe1727b4e6caae89e74e5a4a6654108d6
nujcharee-nhs <78747632+nujcharee-nhs@users.noreply.github.com>
2025-07-09 12:13:24 +0100

    Update definitions.py

--- quickstart/definitions.py
+++ quickstart/definitions.py
@@ -5,7 +5,7 @@ def get_s3_defs():
   from .resources.assumed_role import S3AssumeRoleResource
   defs = Definitions(
 
-        assets=load_assets_from_modules([s3]),
+        assets=load_assets_from_modules([s3, hello_world]),
         resources={
         "s3": S3AssumeRoleResource(
             role_arn="arn:aws:iam::605134447797:role/DataEngineer",
