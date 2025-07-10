d6cbfd595fde7bc6e01cdb27369136840748aedf
nujcharee-nhs <78747632+nujcharee-nhs@users.noreply.github.com>
2025-07-10 09:58:27 +0100

    Update iris.py

--- quickstart/assets/iris.py
+++ quickstart/assets/iris.py
@@ -4,7 +4,7 @@ import boto3
 from datetime import datetime
 
 @asset
-def load_iris_from_s3(context: AssetExecutionContext) -> pd.DataFrame:
+def load_iris_from_s3(context: AssetExecutionContext):
   s3 = boto3.client("s3")
   s3.copy_object(
         Bucket="ukhsa-dev-datalib-ingestion",
