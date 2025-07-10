816408af390a82cf6bff2641a73787a9f2f77ab3
nujcharee-nhs <78747632+nujcharee-nhs@users.noreply.github.com>
2025-07-10 09:20:09 +0100

    Update definitions.py

--- quickstart/definitions.py
+++ quickstart/definitions.py
@@ -1,10 +1,10 @@
 def get_s3_defs():
 
   from dagster import Definitions, load_assets_from_modules
-  from .assets import s3, hello_world, iris
+  from .assets import hello_world, iris
   from .resources.assumed_role import S3AssumeRoleResource
   defs = Definitions(
-    assets=load_assets_from_modules([s3, hello_world, iris]),
+    assets=load_assets_from_modules([hello_world, iris]),
     #     resources={
     #     "s3": S3AssumeRoleResource(
     #         role_arn="arn:aws:iam::605134447797:role/PlatformEngineer",
