a29e59a611e471e956ec6ea5494e7ef4468a6061
nujcharee-nhs <78747632+nujcharee-nhs@users.noreply.github.com>
2025-07-10 08:00:03 +0100

    Update iris.py

--- quickstart/assets/iris.py
+++ quickstart/assets/iris.py
@@ -2,7 +2,7 @@ import pandas as pd
 import dagster as dg
 
 @dg.asset
-def load_csv_from_s3(context: AssetExecutionContext) -> pd.DataFrame:
+def load_iris_from_s3(context: AssetExecutionContext) -> pd.DataFrame:
   s3 = boto3.client("s3")
   s3.copy_object(
         Bucket="ukhsa-dev-datalib-ingestion",
