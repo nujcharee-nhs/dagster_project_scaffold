24b57c656614124d6453564068481987ee60444e
nujcharee-nhs <78747632+nujcharee-nhs@users.noreply.github.com>
2025-07-10 07:50:05 +0100

    Create iris.py

---
+++ quickstart/assets/iris.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import dagster as dg
+
+@dg.asset
+def load_csv_from_s3(context: AssetExecutionContext) -> pd.DataFrame:
+  s3 = boto3.client("s3")
+  s3.copy_object(
+        Bucket="ukhsa-dev-datalib-ingestion",
+        CopySource={
+            "Bucket": "ukhsa-dev-datalib-ingestion",
+            "Key": "dagster/iris.csv"
+        },
+        Key=f"dagster/iris_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+    )
